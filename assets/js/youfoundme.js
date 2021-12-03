69e8e878ccd38d75d17f79e12f5f841200a66052
tech41-de <mathias.dietrich@tech41.de>
2021-12-03 14:06:54 +0100

    adding wallet

--- assets/js/youfoundme.js
+++ assets/js/youfoundme.js
@@ -2,8 +2,9 @@
 const serverUrl = "https://xhuajeqb4esg.usemoralis.com:2053/server";
 const appId = "EKKcMCLp0U2o6l5xCNbuvZmmjCKkyRThrBqzEj9Z";
 
-
 Moralis.start({ serverUrl, appId });
+
+document.getElementById("btn-logout").style.visibility='hidden' ;
 
 /** Add from here down */
 async function login() {
@@ -14,6 +15,12 @@ async function login() {
       alert(JSON.stringify(user));
       console.log(JSON.stringify(user));
       console.log(user.get('ethAddress'));
+
+      document.getElementById("btn-login").style.visibility='hidden';
+      document.getElementById("btn-login").style.display = "none";
+
+      document.getElementById("btn-logout").style.visibility='visible' ;
+     document.getElementById("btn-logout").style.display = '';
      
    } catch(error) {
    	alert("Please enable the MetaMask plugin in your browser and activate your MetaMask wallet")
@@ -25,6 +32,10 @@ async function login() {
 async function logOut() {
   await Moralis.User.logOut();
   console.log("logged out");
+  document.getElementById("btn-login").style.visibility='visible';
+  document.getElementById("btn-login").style.display = '';
+  document.getElementById("btn-logout").style.visibility='hidden' ;
+  document.getElementById("btn-logout").style.display='none' ;
 }
 
 document.getElementById("btn-login").onclick = login;
