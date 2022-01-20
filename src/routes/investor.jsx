7b1b3fee3949015cc0c460556c7b57b1029e6635
tech41-de <mathias.dietrich@tech41.de>
2022-01-20 15:01:02 +0100

    mobile fix

--- src/routes/investor.jsx
+++ src/routes/investor.jsx
@@ -34,6 +34,7 @@ import {
   Head,
   Container,
   Textarea,
+  Center,
 } from '@chakra-ui/react';
 import {
   HamburgerIcon,
@@ -68,83 +69,86 @@ export default function Investor() {
   };
 
   useEffect(() => {
-    if (focusDiv.current) focusDiv.current.focus();
+    // if (focusDiv.current) focusDiv.current.focus();
   });
   return (
-    <center>
-      <Heading>Investor</Heading>
-      <Box padding="4" maxH="1xl">
-        <br />
-        <Text fontSize="2xl">
-          {' '}
-          We love to engage with you.
+    <Flex width="100%">
+      <VStack>
+        <Center>
+          <Heading>Investor</Heading>
+        </Center>
+
+        <Box padding="4" maxH="1xl">
+          <Text fontSize="2xl">
+            <br />
+            Our mission:
+            <br />
+            Youfoundme brings Identity to Web3 and Metaverse,
+            <br />
+            empowering users to work and play together.
+            <br /> <br />
+            Our vision:
+            <br />
+            By 2030 Youfoundme will be a catalyst for groups to operate and
+            interact with other organisations, without the need for notaries,
+            bureaucracy and expensive intermediaries.
+            <br />
+            <br />
+            Phase 1:
+            <br />
+            Career community for early adapters and developers in the Web3
+            space. <br />
+            Connect with talent in the Crypto and Metaverse space for your
+            ventures.
+            <br />
+            <br />
+            Phase 2:
+            <br />
+            As Youfoundme scales with DID adoption we will enable users to
+            authenticate and authorise passwordless across Web3 platforms
+            bridging to oAuth2 for legacy web applications.
+            <br />
+            Proof of identity, effortless sign-on, create verifyable signatures
+            and identity/avatars across the emerging Metaverses.
+            <br /> <br />
+            Phase 3:
+            <br />
+            Allow organisations to run on trust and transparency. Think DID
+            enabled statutes, voting, equity management and copyright
+            protection.
+            <br /> <br />
+            Phase 4:
+            <br />
+            Empower automating trust of processes and contracts between multiple
+            organisations.
+            <br />
+            Think joint-ventures, supply chain automation and decentralized
+            profit sharing.
+            <br />
+            <br />
+            Please contact us
+          </Text>
+          <Textarea
+            ref={focusDiv}
+            onChange={(e) => setInvestor(e.target.value)}
+            value={investor}
+            width="700px"
+            height="200px"
+            fontSize="2xl"></Textarea>
+          <Box h="10px"></Box>
+          <Button onClick={sendInvestor}>send</Button>
+          <Text color="green.500">{investorOk}</Text>
+          <Text color="red.500">{investorError}</Text>
+          <br />
+          or email us at:&nbsp;
+          <CLink href="mailto:mathias.dietrich@youfoundme.io" isExternal>
+            mathias.dietrich@youfundme.io
+          </CLink>
           <br />
           <br />
-          <br />
-          Mission:
-          <br />
-          Youfoundme brings Identity to Web3 and Metaverse,
-          <br />
-          empowering users to work and play together.
-          <br /> <br />
-          Vision:
-          <br />
-          By 2030 youfoundme will be a catalyst for groups to operate and
-          interact with other organisations, without the need for notaries,
-          bureaucracy and expensive intermediaries.
-          <br />
-          <br />
-          Phase 1:
-          <br />
-          Career community for early adapters and developers in the Web3 space.{' '}
-          <br />
-          Connect talent in the Crypto and Metaverse space with recruiters.
-          <br />
-          <br />
-          Phase 2:
-          <br />
-          As Youfoundme scales with DID adoption we will enable users to
-          authenticate and authorise across Web3 platforms.
-          <br />
-          Proof your identity, sign-on effortlessly with QR codes, create
-          verifyable signatures and bring your avatar with you across
-          Metaverses.
-          <br /> <br />
-          Phase 3:
-          <br />
-          Allow organisations to run on trust and transperancy. Think DID
-          enabled statutes, voting, equity management and copyright protection.
-          <br /> <br />
-          Phase 4:
-          <br />
-          Empower automating trust of processes and contracts between multiple
-          organisations.
-          <br />
-          Think joint-ventures, supply chain automation and dezentralized profit
-          sharing.
-          <br />
-          <br /> Please contact us
-        </Text>
-        <Textarea
-          ref={focusDiv}
-          onChange={(e) => setInvestor(e.target.value)}
-          value={investor}
-          width="700px"
-          height="200px"
-          fontSize="2xl"></Textarea>
-        <Box h="10px"></Box>
-        <Button onClick={sendInvestor}>send</Button>
-        <Text color="green.500">{investorOk}</Text>
-        <Text color="red.500">{investorError}</Text>
-        <br /> <br />
-        or email us at:&nbsp;
-        <CLink href="mailto:investor@youfoundme.io" isExternal>
-          investor@youfundme.io
-        </CLink>
-        <br />
-        <br />
-        <Text></Text>
-      </Box>
-    </center>
+          <Text></Text>
+        </Box>
+      </VStack>
+    </Flex>
   );
 }
